6240913fdc7c9e6be5658e2c224846e3d347db9c
mgrzanka <01186969@pw.edu.pl>
2024-10-30 16:29:16 +0100

    fix small issue

--- Sortowanie/insertion_sort.py
+++ Sortowanie/insertion_sort.py
@@ -6,13 +6,14 @@ def insertion_sort(to_sort: list):
         element = to_sort[i]
         j = i - 1
         while element < to_sort[j] and j>=0:
-            to_sort[j], to_sort[j+1] = element, to_sort[j]
+            to_sort[j+1] = to_sort[j]
             j -= 1
+        to_sort[j + 1] = element
     return to_sort
 
 
 if __name__ == '__main__':
-    to_sort = [4, 1, 8, 1, 2, 4]
+    to_sort = [1, 7, 4, 0, -1]
     sorted_list = insertion_sort(to_sort)
     print(sorted_list)
     pass
