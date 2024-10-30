f2757311d3964667d786000e03c228ff26c2edf3
mgrzanka <01186969@pw.edu.pl>
2024-10-30 20:47:00 +0100

    changed buble to bubble and typo in bubble_sort function

--- Sortowanie/buble_sort.py
+++ Sortowanie/buble_sort.py
@@ -1,16 +1,16 @@
 
 
-def buble_sort(to_sort_array: list):
-    n = len(to_sort_array)
+def bubble_sort(to_sort: list):
+    n = len(to_sort)
     for i in range(n):
         for j in range(1, n-i):
             element1, element2 = to_sort[j], to_sort[j-1]
             if element1 < element2:
                 to_sort[j], to_sort[j-1] = element2, element1
-    return to_sort_array
+    return to_sort
 
 
 if __name__ == '__main__':
     to_sort = [5, 1, 10, 5]
-    sorted_list = buble_sort(to_sort)
+    sorted_list = bubble_sort(to_sort)
     print(sorted_list)
